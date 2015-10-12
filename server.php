fbfc7abbebf30465e1529bb976e456a027703aa7
Raphael Rocha <raphaelrocha86@gmail.com>
2015-10-12 14:12:41 -0400

    contabilização da pontuação

--- server.php
+++ server.php
@@ -84,7 +84,7 @@ if(isset($_POST['method'])){
 	else if(strcmp('verifica-rodada', $_POST['method']) == 0){
 		$arrayResponse = array();
 
-		list($idJogo,$slot) = explode(";",$_POST['data']);
+		list($idJogo,$slot,$idRodada) = explode(";",$_POST['data']);
 
 		$sql = "select *,rodada.id as id_rodada from rodada join jogo on (jogo.id = rodada.id_jogo)
 				where jogo.id = $idJogo and (rodada.jogada_jogador_1 is null or rodada.jogada_jogador_2 is null)";
@@ -98,16 +98,22 @@ if(isset($_POST['method'])){
 			}
 			$sql = null;
 			$result = null;
-			$sql = "select count(*) as nRodada from rodada join jogo on (jogo.id = rodada.id_jogo)
+			$sql = "select count(*) as nRodada, sum(pontuacao_jogador_1) pt_j1, sum(pontuacao_jogador_2) pt_j2 from rodada join jogo on (jogo.id = rodada.id_jogo)
 					where jogo.id = $idJogo;";
 			$result = $conn->query($sql);		
 			if ($result->num_rows > 0) {
 				$nRodada;
+				$pt_j1;
+				$pt_j2;
 				foreach($result as $model){
 					$nRodada = $model["nRodada"];
+					$pt_j1 = $model["pt_j1"];
+					$pt_j2 = $model["pt_j2"];
 				}
 				array_push($arrayResponse, array('id_rodada'=>$idRodada,
-												 'n_rodada'=>$nRodada));
+													 'n_rodada'=>$nRodada,
+													 'pt_slot_1'=>$pt_j1,
+													 'pt_slot_2'=>$pt_j2));
 				echo json_encode($arrayResponse);
 			}else{
 				array_push($arrayResponse, array('id'=>"erro-ao-contar-rodadas [".$conn->error."]"));
@@ -115,22 +121,61 @@ if(isset($_POST['method'])){
 			}
 			
 		}else{
+			if($idRodada!="0"){
+				$sql = "select * from rodada where id=$idRodada";
+				$result = $conn->query($sql);		
+				if ($result->num_rows > 0) {
+					$jogada_j1;
+					$jogada_j2;
+					$pt_j1;
+					$pt_j2;
+					foreach($result as $model){
+						$jogada_j1 = $model["jogada_jogador_1"];
+						$jogada_j2 = $model["jogada_jogador_2"];
+					}
+
+					if(($jogada_j1=="0")&&($jogada_j2=="0")){
+						$pt_j1=1;
+						$pt_j2=1;
+					}else if(($jogada_j1=="1")&&($jogada_j2=="0")){
+						$pt_j1=-2;
+						$pt_j2=2;
+					}else if(($jogada_j1=="0")&&($jogada_j2=="1")){
+						$pt_j1=2;
+						$pt_j2=-2;
+					}else if(($jogada_j1=="1")&&($jogada_j2=="1")){
+						$pt_j1=-1;
+						$pt_j2=-1;
+					}
+				}
+				$sql = "update rodada set pontuacao_jogador_1 = $pt_j1, pontuacao_jogador_2 = $pt_j2
+						where id=$idRodada";
+				if ($conn->query($sql) === TRUE) {
+					//salva s pontos
+				}
+			}
 			$sql = "insert into rodada values (NULL,$idJogo,NULL,NULL,0,0,NULL)";
 			if ($conn->query($sql) === TRUE) {
 				$idRodada = $conn->insert_id;
 
 				$sql = null;
 				$result = null;
-				$sql = "select count(*) as nRodada from rodada join jogo on (jogo.id = rodada.id_jogo)
+				$sql = "select count(*) as nRodada, sum(pontuacao_jogador_1) pt_j1, sum(pontuacao_jogador_2) pt_j2  from rodada join jogo on (jogo.id = rodada.id_jogo)
 						where jogo.id = $idJogo;";
 				$result = $conn->query($sql);		
 				if ($result->num_rows > 0) {
 					$nRodada;
+					$pt_j1;
+					$pt_j2;
 					foreach($result as $model){
 						$nRodada = $model["nRodada"];
+						$pt_j1 = $model["pt_j1"];
+						$pt_j2 = $model["pt_j2"];
 					}
 					array_push($arrayResponse, array('id_rodada'=>$idRodada,
-													 'n_rodada'=>$nRodada));
+													 'n_rodada'=>$nRodada,
+													 'pt_slot_1'=>$pt_j1,
+													 'pt_slot_2'=>$pt_j2));
 					echo json_encode($arrayResponse);
 				}else{
 					array_push($arrayResponse, array('id'=>"erro-ao-contar-rodadas [".$conn->error."]"));
@@ -167,7 +212,6 @@ if(isset($_POST['method'])){
 	
 	}
 	else{
-		
 		$arrayResponse = array();
 
 		array_push($arrayResponse, array('id'=>'acao-desconhecida'));
